fad01d65d4a21902cc468fa871980a6ae6c155a9
camilo-camargo <camilocamargo49@gmail.com>
2024-04-08 04:11:08 -0500

    feat: Add empty open window

--- main.c
+++ main.c
@@ -1,5 +1,48 @@
+#include "main.h"
+#include "SDL_error.h"
+#include "SDL_surface.h"
+#include "SDL_video.h"
+#include <SDL.h>
+#include <SDL_events.h>
+#include <SDL_pixels.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
+
+int main(void) {
+  SDL_Window *window = NULL;
+  SDL_Surface *surface = NULL;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    // TODO: Add loggers with custom profiles
+    fprintf(stderr, "SDL couldn't initiate %s\n", SDL_GetError());
+    return -1;
+  }
+
+  window = SDL_CreateWindow(PROGRAM_TITLE, SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    fprintf(stderr, "SDL couldn't not create window:  %s\n", SDL_GetError());
+    return -1;
+  }
+
+  surface = SDL_GetWindowSurface(window);
+
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+  SDL_Event e;
+  bool is_open = true;
+  while (is_open) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        is_open = false;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return EXIT_SUCCESS;
 }
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef PHYSYCS_ENGINE
+  #define PROGRAM_TITLE "Physics Engine"
+  #define SCREEN_WIDTH 640
+  #define SCREEN_HEIGHT 480
+  #define ASPECT_RATIO_16_9 9/16
+#endif
